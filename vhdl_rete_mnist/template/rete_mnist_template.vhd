2896125a705b091d8b0e3bcb158a4c44bca898f8
Mario Vitaglione <77239918+marvitaglione@users.noreply.github.com>
2022-11-14 15:58:43 +0100

    update

--- vhdl_rete_mnist/template/rete_mnist_template.vhd
+++ vhdl_rete_mnist/template/rete_mnist_template.vhd
@@ -25,8 +25,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-use IEEE.math_real."log2";
-use IEEE.math_real."ceil";
+use IEEE.math_real.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
